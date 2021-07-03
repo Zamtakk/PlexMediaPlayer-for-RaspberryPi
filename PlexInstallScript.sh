b263a707be4cd678e100f12ce95caefa51fbfc4d
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2021-07-03 10:16:26 +0200

    Changed commands to work with specific directories

--- PlexInstallScript.sh
+++ PlexInstallScript.sh
@@ -20,24 +20,25 @@ echo "Installing needed dependencies"
 apt-get install -y autoconf automake libtool libharfbuzz-dev libfreetype6-dev libfontconfig1-dev libx11-dev libxrandr-dev libvdpau-dev libva-dev mesa-common-dev libegl1-mesa-dev yasm libasound2-dev libpulse-dev libuchardet-dev zlib1g-dev libfribidi-dev git libgnutls28-dev libgl1-mesa-dev libsdl2-dev cmake python3 python python-minimal git mpv libmpv-dev
 
 echo "Downloading QT for Plex"
-cd /home/pi
-runuser -l pi -c 'wget https://github.com/koendv/qt5-opengl-raspberrypi/releases/download/v5.12.5-1/qt5-opengl-dev_5.12.5_armhf.deb'
+runuser -l pi -c 'wget https://github.com/koendv/qt5-opengl-raspberrypi/releases/download/v5.12.5-1/qt5-opengl-dev_5.12.5_armhf.deb -P /home/pi'
 
 echo "Installing QT for Plex"
-apt-get install -y ./qt5-opengl-dev_5.12.5_armhf.deb
+apt-get install -y /home/pi/qt5-opengl-dev_5.12.5_armhf.deb
+
+echo "Removing Qt installer"
 runuser -l pi -c 'rm qt5-opengl-dev_5.12.5_armhf.deb'
-runuser -l pi -c 'mkdir /home/pi/pmp'
-cd /home/pi/pmp
+
 echo "Cloning Plex"
+runuser -l pi -c 'mkdir /home/pi/pmp'
 runuser -l pi -c 'git clone git://github.com/plexinc/plex-media-player /home/pi/pmp'
-cd /home/pi/pmp/plex-media-player
-runuser -l pi -c 'mkdir build'
-cd /home/pi/pmp/plex-media-player/build
+
 echo "Building Plex"
-runuser -l pi -c 'cmake -DCMAKE_BUILD_TYPE=Debug -DQTROOT=/usr/lib/qt5.12/ -DCMAKE_INSTALL_PREFIX=/usr/local/ ..'
-runuser -l pi -c 'make -j4'
+runuser -l pi -c 'mkdir /home/pi/pmp/plex-media-player/build'
+runuser -l pi -c 'cmake -DCMAKE_BUILD_TYPE=Debug -DQTROOT=/usr/lib/qt5.12/ -DCMAKE_INSTALL_PREFIX=/usr/local/ -B/home/pi/pmp/plex-media-player/build -S/home/pi/pmp/plex-media-player'
+runuser -l pi -c 'make -j4 -C /home/pi/pmp/plex-media-player/build'
+
 echo "Installing Plex"
-make install
+make install -C /home/pi/pmp/plex-media-player/build
 
 echo "Creating automatic startup for Plex"
 runuser -l pi -c 'touch /home/pi/plex_startup.sh'
